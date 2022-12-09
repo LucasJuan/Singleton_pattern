207e9431d24d3dec9b81a37b7d3815a017881ff0
Lucas Juan <ljgsodre@outlook.com>
2022-12-09 10:16:05 -0300

    Add project files.

---
+++ Singleton_pattern/Model/SingletonClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_pattern.Model
+{
+    public class SingletonClass
+    {
+        //principle of singleton pattern you ought create  an object, and make the constructor private to the class not be instantiated
+        private static SingletonClass instance = new SingletonClass();
+        private SingletonClass() { }
+
+        public static SingletonClass getInstance()
+        {
+            return instance;
+        }
+
+        public void showMessage()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Singleton_pattern/Program.cs
@@ -0,0 +1,6 @@
+﻿using Singleton_pattern.Model;
+    //as the constructor is not available  here you cannot instantiate, use the static
+    //class to acces in the entire application with the same instance
+    var item = SingletonClass.getInstance();
+   //show the message
+    item.showMessage();
